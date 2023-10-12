ceed8b974ab8941b8d3270b551a6f6b79dd4b165
yeabtsega45 <yabtsega2022@gmail.com>
2023-10-12 12:22:37 +0300

    fetching data with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
+// import { useState } from "react";
 // import './App.css'
-import Header from './components/Header'
-import Form from './components/Form'
-import AddCourse from './components/AddCourse'
-import DisplayCourse from './components/DisplayCourse'
-
+import Header from "./components/Header";
+// import Form from './components/Form'
+import AddCourse from "./components/AddCourse";
+// import DisplayCourse from './components/DisplayCourse'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='container flex flex-col justify-center mx-auto'>
+      <div className="container flex flex-col justify-center mx-auto">
         <Header />
-        <div className='container flex justify-between px-12  space-x-12 mt-12'>
+        <div className="container flex justify-between px-12  space-x-12 mt-12">
           <AddCourse />
           {/* <DisplayCourse />
           <Form /> */}
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
