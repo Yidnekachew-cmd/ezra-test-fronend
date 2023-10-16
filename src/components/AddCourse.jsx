06d3d35635fb103868be311426e3fe1da66a4692
AmanWorku <amexworku2014@gmail.com>
2023-10-16 16:24:52 +0300

    For Yazkeba and Bese

--- src/components/AddCourse.jsx
+++ src/components/AddCourse.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import DisplayCourse from "./DisplayCourse";
 import Form from "./Form";
 import AddCourseImage from "./AddCourseImage";
