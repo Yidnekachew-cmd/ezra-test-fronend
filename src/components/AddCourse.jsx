75d58509e99af1e54e2175dad905ee3540318afd
yeabtsega45 <yabtsega2022@gmail.com>
2023-10-31 16:07:49 +0300

    creating quizes and slides

--- src/components/AddCourse.jsx
+++ src/components/AddCourse.jsx
@@ -1,13 +1,11 @@
 import SubTitle from "./SubTitle";
 import ImgMap from "./ImgMap";
-import Quiz from "./Quiz";
 
 const AddCourse = () => {
   return (
     <div className="container">
       <ImgMap /> {/* This is the data displayed that comes from backend */}
       <SubTitle /> {/* This is the form to add courses */}
-      <Quiz />
     </div>
   );
 };
