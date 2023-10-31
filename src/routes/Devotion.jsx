85fdfce22a813188e2841a481c0ee60065ad8e2b
Yidne <yamiyekn@gmail.com>
2023-10-31 13:46:31 +0300

    Add Router packages and Create routes component to store routes.

---
+++ src/routes/Devotion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Devotion = () => {
+  return (
+    <div>Devotion</div>
+  )
+}
+
+export default Devotion
---
+++ src/routes/SabbathSchool.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SabbathSchool = () => {
+  return (
+    <div>SabbathSchool</div>
+  )
+}
+
+export default SabbathSchool
