0ed9b5a375786526a817810350191d156575015f
yeabtsega45 <yabtsega2022@gmail.com>
2023-11-21 20:27:19 +0300

    creating form and storing on state

--- src/components/CreateCourse.jsx
+++ src/components/CreateCourse.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 
@@ -12,22 +12,24 @@ function CreateCourse() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(data);
     const formdata = new FormData();
     formdata.append("title", data.title);
     formdata.append("description", data.description);
     formdata.append("image", data.image);
-    axios
-      .post("/course/create", formdata)
-      .then((res) => {
-        // navigate("/course/add");
-        console.log(res);
-      })
-      .catch((err) => console.log(err));
+    console.log(formdata);
+    // axios
+    //   .post("/course/create", formdata)
+    //   .then((res) => {
+    //     navigate("/course/add");
+    //     console.log(res);
+    //   })
+    //   .catch((err) => console.log(err));
   };
 
   return (
     <div className="h-screen pt-9 px-20">
-      <h2 className="text-orange-500 border-b-4 border-gray-300 pb-1">
+      <h2 className="text-orange-500 text-2xl font-bold border-b-4 border-gray-300 pb-1">
         Create Course
       </h2>
       <form
@@ -57,12 +59,20 @@ function CreateCourse() {
           <label className="block text-orange-500">Description</label>
           <input
             type="text"
-            className="w-full px-3 py-2 text-orange-500 leading-tight border border-orange-300 rounded-md focus:outline-none focus:border-blue-500"
+            className="w-full px-3 pt-2 pb-12 text-orange-500 leading-tight border border-orange-300 rounded-md focus:outline-none focus:border-blue-500"
             name="description"
             placeholder="Add a description"
             autoComplete="off"
             onChange={(e) => setData({ ...data, description: e.target.value })}
           />
+        </div>
+        <div className="col-span-12">
+          <button
+            type="submit"
+            className="px-4 py-2 font-semibold text-white bg-orange-500 rounded-md hover:bg-orange-600"
+          >
+            Create
+          </button>
         </div>
       </form>
     </div>
