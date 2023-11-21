168f7b249da8edf9b7338e12ab34776b846bdbd6
AmanWorku <amexworku2014@gmail.com>
2023-11-21 11:28:45 +0300

    Give styling to the add chapter form

--- src/components/ChapterForm.jsx
+++ src/components/ChapterForm.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 const ChapterForm = ({ handleAddChapter }) => {
   const [title, setTitle] = useState('');
@@ -14,11 +20,21 @@ const ChapterForm = ({ handleAddChapter }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Chapter Title: </label>
-      <Input type="text" className="w-64" value={title} onChange={(e) => setTitle(e.target.value)} required />
-      <Button variant="outline" type="submit" className="my-2"> Add Chapter </Button>
-    </form>
+    <>
+    <Card className="w-[350px] my-4">
+      <CardHeader>
+        <CardTitle>Create chapter</CardTitle>
+        <CardDescription>Create a new chapter for the course.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          <label>Chapter Title: </label>
+            <Input type="text" className="w-64" value={title} onChange={(e) => setTitle(e.target.value)} required />
+            <Button variant="outline" type="submit" className="my-2"> Add Chapter </Button>
+        </form>
+      </CardContent>
+    </Card>
+    </>
   );
 };
 
