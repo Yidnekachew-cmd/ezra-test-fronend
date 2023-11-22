dab7a9bb5c050568288169178c904467f473b290
yeabtsega45 <yabtsega2022@gmail.com>
2023-11-22 15:35:13 +0300

    creating the courses available page on figma user design

---
+++ src/features/CourseComponents/CoursesAvailable.jsx
@@ -0,0 +1,11 @@
+function CoursesAvailable() {
+  return (
+    <div className="h-screen pt-9 px-20">
+      <h2 className="text-orange-500 text-2xl font-bold border-b-4 border-gray-300 pb-1">
+        Courses Available
+      </h2>
+    </div>
+  );
+}
+
+export default CoursesAvailable;
--- src/routes/Courses.jsx
+++ src/routes/Courses.jsx
@@ -1,9 +1,11 @@
 import CreateCourse from "@/components/CreateCourse";
+import CoursesAvailable from "@/features/CourseComponents/CoursesAvailable";
 import AddCourse from "../components/AddCourse";
 
 const Courses = () => {
   return (
     <div>
+      <CoursesAvailable />
       <CreateCourse />
       <AddCourse />
     </div>
