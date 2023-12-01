567fff191edba11491885c47cdc1bd35e98d50ed
Besufekad-HAZ <besurye@gmail.com>
2023-12-01 10:32:54 +0300

    Add login and signup componenents with css and and context components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 import Header from "./components/Header";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./routes/Home";
 import Courses from "./routes/Courses";
 import SabbathSchool from "./routes/SabbathSchool";
 import Devotion from "./routes/Devotion";
 import AboutUs from "./routes/AboutUs";
 import NotMatch from "./routes/NotMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/sabbathSchool" element={<SabbathSchool />} />
-        <Route path="/devotion" element={<Devotion />} />
-        <Route path="/aboutUs" element={<AboutUs />} />
-        <Route path="*" element={<NotMatch />} />
-      </Routes>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!user ? <Signup /> : <Navigate to="/" />}
+          />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/sabbathSchool" element={<SabbathSchool />} />
+          <Route path="/devotion" element={<Devotion />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path="*" element={<NotMatch />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
