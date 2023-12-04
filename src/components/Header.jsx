536f1ad88fcef6427c492f5f32219ccabdd2daee
Yidne <yamiyekn@gmail.com>
2023-12-04 16:16:18 +0300

    Add Hero and Intro components and style them.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container flex justify-between mt-6 px-12 items-center">
+    <header className="container px-12 bg-header-img  bg-bottom bg-cover">
+      <div className=" flex justify-between py-6 items-center text-white font-nokia-bold w-[80%] mx-auto">
         <div className=" cursor-pointer ">
           <h3>
             <strong className="text-2xl">Ezra</strong> Seminary
           </h3>
         </div>
         <nav>
-          <ul className="hidden md:flex space-x-4 cursor-pointer">
+          <ul className="hidden md:flex space-x-4 cursor-pointer text-sm justify-center items-center">
             <li className="hover:text-gray-400">
               <NavLink to="/">Home</NavLink>
             </li>
@@ -32,8 +32,10 @@ const Header = () => {
             <li className="hover:text-gray-400">
               <NavLink to="/logIn">Log In</NavLink>
             </li>
-            <li className="hover:text-gray-400">
-              <NavLink to="/createAccount">Create Account</NavLink>
+            <li className="hover:text-gray-400 text-base ">
+              <NavLink to="/createAccount">
+                <button type="button" className="bg-[#EA9215] rounded-full py-1 px-2">Create Account</button>
+                </NavLink>
             </li>
           </ul>
         </nav>
