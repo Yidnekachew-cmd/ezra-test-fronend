6be06965dc1b09d870b669896f159e8bb6cd0059
yeabtsega45 <yabtsega2022@gmail.com>
2023-12-04 16:46:56 +0300

    working on elements sidebar

--- src/features/CourseComponents/AdminChapter.jsx
+++ src/features/CourseComponents/AdminChapter.jsx
@@ -1,14 +1,38 @@
+// import axios from "axios";
+import { useSelector } from "react-redux";
 import ChaptersAdd from "./ChaptersAdd";
 import ElementsAdd from "./ElementsAdd";
 
 function AdminChapter() {
+  const { title, description, image } = useSelector((state) => state.course);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("image", image);
+
+    // axios
+    //   .post("/course/create", formData)
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  };
+
   return (
     <div>
       <div className="flex justify-between border-gray-200 border-2 p-1">
         <button className="text-white font-bold text-3xl bg-orange-400 hover:bg-orange-500 rounded-[50%]">
           <span className="material-symbols-outlined t">arrow_left</span>
         </button>
-        <button className="px-2 font-semibold text-white bg-orange-500 rounded-md hover:bg-orange-600">
+        <button
+          onSubmit={handleSubmit}
+          className="px-2 font-semibold text-white bg-orange-500 rounded-md hover:bg-orange-600"
+        >
           Publish
         </button>
       </div>
--- src/features/CourseComponents/ElementsAdd.jsx
+++ src/features/CourseComponents/ElementsAdd.jsx
@@ -1,18 +1,65 @@
+import { useState } from "react";
+
 function ElementsAdd() {
+  const [elements, setElements] = useState([]);
+  const [currentElement, setCurrentElement] = useState("");
+
+  const handleDropdownChange = (e) => {
+    setCurrentElement(e.target.value);
+  };
+
+  const handleAddButtonClick = () => {
+    if (currentElement) {
+      setElements([
+        ...elements,
+        {
+          type: currentElement,
+          id: `${currentElement}${elements.length + 1}`,
+          value: "",
+        },
+      ]);
+      setCurrentElement(""); // Reset dropdown if needed
+    }
+    console.log(elements);
+  };
+
+  const handleInputChange = (id, value) => {
+    setElements(
+      elements.map((el) => (el.id === id ? { ...el, value: value } : el))
+    );
+  };
+
   return (
     <div className="bg-white w-[16%] p-6">
       <p>Insert Element</p>
       <div>
-        <select name="elements" id="elements">
+        <select
+          name="elements"
+          id="elements"
+          value={currentElement}
+          onChange={handleDropdownChange}
+        >
           <option value="">Choose Type</option>
           <option value="title">title</option>
           <option value="sub">sub</option>
           <option value="img">img</option>
         </select>
-        <button className="px-2 font-semibold text-white bg-orange-500 rounded-md hover:bg-orange-600">
+        <button
+          onClick={handleAddButtonClick}
+          className="px-2 font-semibold text-white bg-orange-500 rounded-md hover:bg-orange-600"
+        >
           Add
         </button>
       </div>
+      {elements.map((element, index) => (
+        <input
+          key={index}
+          id={element.id}
+          placeholder={`Enter ${element.type}`}
+          value={element.value}
+          onChange={(e) => handleInputChange(element.id, e.target.value)}
+        />
+      ))}
     </div>
   );
 }
