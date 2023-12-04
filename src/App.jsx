cf9deb6cad56835e22e9bfd7c97e36fb41e3e757
Yidne <yamiyekn@gmail.com>
2023-12-04 21:32:01 +0300

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ import Footer from "./components/Footer";
 import AddCourse from "./components/AddCourse";
 import ChaptersDisplay from "./features/CourseComponents/ChaptersDisplay";
 import SlidesDisplay from "./features/CourseComponents/SlidesDisplay";
+import AdminChapter from "./features/CourseComponents/AdminChapter";
 
 function App() {
   const { user } = useAuthContext();
   return (
     <>
-<<<<<<< HEAD
       <BrowserRouter>
         <Header />
         <Routes>
@@ -48,30 +48,11 @@ function App() {
             path="/courses/get/:courseId/chapter/:chapterId"
             element={<SlidesDisplay />}
           />
+          <Route path="/courses/create/chapters" element={<AdminChapter />} />
           <Route path="*" element={<NotMatch />} />
         </Routes>
         <Footer />
       </BrowserRouter>
-=======
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/course/create/add" element={<AddCourse />} />
-        <Route path="/sabbathSchool" element={<SabbathSchool />} />
-        <Route path="/devotion" element={<Devotion />} />
-        <Route path="/aboutUs" element={<AboutUs />} />
-        <Route path="/contactUs" element={<ContactUs />} />
-        <Route path="/logIn" element={<LogIn />} />
-        <Route path="/createAccount" element={<CreateAccount />} />
-        <Route path="/courses/get/:courseId" element={<ChaptersDisplay />} />
-        <Route
-          path="/courses/get/:courseId/chapter/:chapterId"
-          element={<SlidesDisplay />}
-        />
-        <Route path="*" element={<NotMatch />} />
-      </Routes>
->>>>>>> 532ac8e ( Add ContactUs, LogIn and CreateAccount componets.)
     </>
   );
 }
