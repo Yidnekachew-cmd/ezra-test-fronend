9d233c906e8223ae97b3fb7ab54f9abbe6b6b41f
AmanWorku <amexworku2014@gmail.com>
2023-12-06 16:55:53 +0300

    Add some styling to pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,8 +14,22 @@ const Login = () => {
   };
 
   return (
-    <form className="login" onSubmit={handleSubmit}>
-      <h3 className="titles">Login</h3>
+    <div className="flex w-[70%] rounded-xl border-2 border-accent-6 mx-auto my-20">
+      <div className="flex flex-col bg-coming-soon bg-cover w-[60%] font-nokia-bold p-7 justify-between text-white rounded-xl" style={{backgroundPositionX:"-80px"}}>
+      <div className="flex space-x-3 cursor-pointer text-white ">
+          <img src="src/assets/ezra-logo.svg" alt="" />
+          <h3 className="self-center text-2xl font-Lato-Regular">
+            <span className="font-Lato-Bold">EZRA</span> Seminary
+          </h3>
+        </div>
+        <p className="text-2xl">
+        መጽሃፍ ቅዱስ እግዚአብሔርን<br/>
+        <span className="text-3xl text-accent-6">በግላችን የምናውቅበት</span><br/>
+        ዋነኛው መንገድ ነው።<br/>
+        </p>
+      </div>
+    <form className="font-nokia-bold px-4 py-5" onSubmit={handleSubmit}>
+      <h3 className="text-2xl">Login</h3>
 
       <label>Email:</label>
       <input
@@ -36,6 +50,7 @@ const Login = () => {
       <button disabled={isLoading}>Login</button>
       {error && <div className="error">{error}</div>}
     </form>
+    </div>
   );
 };
 
