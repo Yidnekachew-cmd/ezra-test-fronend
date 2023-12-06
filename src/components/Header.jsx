30f1a26c14c144c2ec745f80796918858293e434
Besufekad-HAZ <besurye@gmail.com>
2023-12-06 13:59:13 +0300

    fix add account and add a user icon with a drop down menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "./LogoutButton";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const Header = () => {
   const { user } = useAuthContext();
+  const [showAccountModal, setShowAccountModal] = useState(false);
+
+  const handleAccountClick = () => {
+    setShowAccountModal(!showAccountModal);
+  };
+
   return (
     <header className="bg-header-img bg-bottom bg-cover backdrop-blur-md">
-      <div className=" flex justify-between py-6 items-center text-white font-nokia-bold w-[80%] mx-auto">
-        <div className="flex justify-center items-center space-x-3 cursor-pointer ">
+      <div className="flex justify-between py-6 items-center text-white font-nokia-bold w-[80%] mx-auto">
+        <div className="flex justify-center items-center space-x-3 cursor-pointer">
           <img src="src/assets/ezra-logo.svg" alt="" />
           <h3>
             <strong className="text-2xl">Ezra</strong> Seminary
@@ -34,17 +42,46 @@ const Header = () => {
               <NavLink to="/contactUs">Contact Us</NavLink>
             </li>
             {user ? (
-              <LogoutButton />
-            ) : (
-              <li className="hover:text-gray-400">
-                <NavLink to="/logIn">Log In</NavLink>
+              <li className="relative">
+                <button
+                  onClick={handleAccountClick}
+                  className="bg-accent-6 hover:bg-accent-7 text-base focus:outline-none"
+                >
+                  <FaRegUserCircle user={user} />
+
+                  {/* {user.email}  role will be added here */}
+                </button>
+                {showAccountModal && (
+                  <div className="absolute top-[40px] right-0 bg-accent-6 shadow-lg rounded-md z-10">
+                    <div className="px-4 py-2 border-b">
+                      Logged in as: {user.email}
+                    </div>
+                    <div className="px-4 py-2 border-b">
+                      <NavLink to="/profile">Profile</NavLink>
+                    </div>
+                    <div className="px-4 py-2">
+                      <LogoutButton />
+                    </div>
+                  </div>
+                )}
               </li>
+            ) : (
+              <>
+                <li className="hover:text-gray-400">
+                  <NavLink to="/logIn">Log In</NavLink>
+                </li>
+                <li className="hover:text-gray-400 text-base ">
+                  <NavLink to="/signup">
+                    <button
+                      type="button"
+                      className="font-nokia-bold bg-accent-6 rounded-full py-2 px-5 hover:bg-accent-7 text-white transition-colors"
+                    >
+                      Create Account
+                    </button>
+                  </NavLink>
+                </li>
+              </>
             )}
-            <li className="hover:text-gray-400 text-base ">
-              <NavLink to="/createAccount">
-                <button type="button" className="font-nokia-bold bg-accent-6 rounded-full py-2 px-5 hover:bg-accent-7 text-white transition-colors">Create Account</button>
-                </NavLink>
-            </li>
           </ul>
         </nav>
       </div>
