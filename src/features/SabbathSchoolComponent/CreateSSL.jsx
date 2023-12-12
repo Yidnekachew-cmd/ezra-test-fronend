2ed96cdb9c005a4dbdb3df502d487e45cf5ee06b
AmanWorku <amexworku2014@gmail.com>
2023-12-12 16:00:17 +0300

    Create a simple form

--- src/features/SabbathSchoolComponent/CreateSSL.jsx
+++ src/features/SabbathSchoolComponent/CreateSSL.jsx
@@ -1,5 +1,37 @@
 const CreateSSL = () => {
-  return <div>CreateSSL</div>;
+  return (
+    <div className="flex gap-4 w-[80%] h-screen mx-auto my-4">
+      <div className="w-[70%] border border-accent-6 rounded-lg">
+        <p className="px-2 py-2 font-nokia-bold text-xl text-accent-6">
+          Create Sabbath School Lesson
+        </p>
+        <div className="flex justify-between">
+          <div className="">
+            <select>
+              <option value="1">የእግዚአብሔርን ተልዕኮ...</option>
+              <option value="1">የእግዚአብሔርን ተልዕኮ...</option>
+              <option value="1">የእግዚአብሔርን ተልዕኮ...</option>
+            </select>
+            <select>
+              <option value="1">Week 1</option>
+              <option value="1">Week 2</option>
+              <option value="1">Week 3</option>
+            </select>
+            <select>
+              <option value="1">Sabbath</option>
+              <option value="1">Day 1</option>
+              <option value="1">Day 2</option>
+              <option value="1">Day 3</option>
+              <option value="1">Day 4</option>
+              <option value="1">Day 5</option>
+              <option value="1">Further Thought</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="w-[30%] border border-accent-6 rounded-lg"></div>
+    </div>
+  );
 };
 
 export default CreateSSL;
--- src/routes/SabbathSchool.jsx
+++ src/routes/SabbathSchool.jsx
@@ -1,16 +1,26 @@
+import SSLHome from "@/features/SabbathSchoolComponent/SSLHome";
+import CreateSSL from "@/features/SabbathSchoolComponent/CreateSSL";
+
 const SabbathSchool = () => {
   return (
-    <div className='relative h-screen'>
-      <div className='bg-coming-soon bg-cover h-full' style={{ backgroundImage: `url('./src/assets/home-page-img.svg')` }}></div>
-      <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center pointer-events-none'>
-        <div className='absolute top-0 left-0 w-full h-full bg-black opacity-40'></div>
-        <div className="relative z-10 text-primary-1 align-middle font-bold text-7xl text-center">
-          <div>Sabbath School</div>
-          <div className="text-3xl">Coming Soon</div>
-        </div>
-      </div>
-    </div>
-  )
-}
+    // <div className="relative h-screen">
+    //   <div
+    //     className="bg-coming-soon bg-cover h-full"
+    //     style={{ backgroundImage: `url('./src/assets/home-page-img.svg')` }}
+    //   ></div>
+    //   <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center pointer-events-none">
+    //     <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
+    //     <div className="relative z-10 text-primary-1 align-middle font-bold text-7xl text-center">
+    //       <div>Sabbath School</div>
+    //       <div className="text-3xl">Coming Soon</div>
+    //     </div>
+    //   </div>
+    // </div>
+    <>
+      <SSLHome />
+      <CreateSSL />
+    </>
+  );
+};
 
 export default SabbathSchool;
