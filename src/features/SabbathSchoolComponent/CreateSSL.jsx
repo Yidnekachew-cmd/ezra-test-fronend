849433e5664054930bfe95ad5f20d9edb0142113
AmanWorku <amexworku2014@gmail.com>
2023-12-12 16:42:44 +0300

    Create ssl form

--- src/features/SabbathSchoolComponent/CreateSSL.jsx
+++ src/features/SabbathSchoolComponent/CreateSSL.jsx
@@ -1,23 +1,24 @@
+import { PlusCircle } from "@phosphor-icons/react";
+import SSLForm from "./SSLForm";
+
 const CreateSSL = () => {
   return (
-    <div className="flex gap-4 w-[80%] h-screen mx-auto my-4">
-      <div className="w-[70%] border border-accent-6 rounded-lg">
-        <p className="px-2 py-2 font-nokia-bold text-xl text-accent-6">
-          Create Sabbath School Lesson
-        </p>
-        <div className="flex justify-between">
-          <div className="">
-            <select>
+    <div className="flex gap-4 w-[80%] h-screen mx-auto my-4 font-nokia-bold">
+      <div className="w-[70%] border border-accent-6 rounded-lg px-4 py-4">
+        <p className="text-xl text-accent-6">Create Sabbath School Lesson</p>
+        <div className="flex justify-between px-4">
+          <div className="flex gap-4 mt-2 text-secondary-6">
+            <select className="border border-accent-6 px-2 py-1 rounded-sm">
               <option value="1">የእግዚአብሔርን ተልዕኮ...</option>
               <option value="1">የእግዚአብሔርን ተልዕኮ...</option>
               <option value="1">የእግዚአብሔርን ተልዕኮ...</option>
             </select>
-            <select>
+            <select className="border border-accent-6 px-2 py-1 rounded-sm">
               <option value="1">Week 1</option>
               <option value="1">Week 2</option>
               <option value="1">Week 3</option>
             </select>
-            <select>
+            <select className="border border-accent-6 px-2 py-1 rounded-sm">
               <option value="1">Sabbath</option>
               <option value="1">Day 1</option>
               <option value="1">Day 2</option>
@@ -27,9 +28,20 @@ const CreateSSL = () => {
               <option value="1">Further Thought</option>
             </select>
           </div>
+          <div className="flex gap-2 text-sm">
+            <button className="flex items-center gap-1 text-primary-1 bg-accent-6 px-2 py-1 rounded-sm">
+              Create Week <PlusCircle />
+            </button>
+            <button className="flex items-center gap-1 text-primary-1 bg-accent-6 px-2 py-1 rounded-sm">
+              Create Day <PlusCircle />
+            </button>
+          </div>
         </div>
+        <hr className="mt-4" />
       </div>
-      <div className="w-[30%] border border-accent-6 rounded-lg"></div>
+      <div className="w-[30%] border border-accent-6 rounded-lg">
+        <SSLForm />
+      </div>
     </div>
   );
 };
---
+++ src/features/SabbathSchoolComponent/SSLForm.jsx
@@ -0,0 +1,11 @@
+const SSLForm = () => {
+  return (
+    <div className="flex flex-col p-4 text-accent-6">
+      <form>
+        <input type="date" className="border border-accent-6 p-2 rounded-sm" />
+      </form>
+    </div>
+  );
+};
+
+export default SSLForm;
