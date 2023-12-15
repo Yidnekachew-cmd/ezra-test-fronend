b7c5b4ac18df437d777979d648f6d63754986f2c
Besufekad-HAZ <besurye@gmail.com>
2023-12-15 17:13:08 +0300

    Routes fixes and First functional phase of the Admin Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./routes/Home";
@@ -6,8 +7,8 @@ import SabbathSchool from "./routes/SabbathSchool";
 import Devotion from "./routes/Devotion";
 import AboutUs from "./routes/AboutUs";
 import ContactUs from "./routes/ContactUs";
-import LogIn from "./routes/LogIn";
-import CreateAccount from "./routes/CreateAccount";
+// import LogIn from "./routes/LogIn";
+// import CreateAccount from "./routes/CreateAccount";
 import NotMatch from "./routes/NotMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -22,68 +23,93 @@ import AdminDashboard from "./routes/AdminDashboard";
 
 function App() {
   const { user } = useAuthContext();
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/login"
-            element={
-              !user ? (
-                <Login />
-              ) : user.role === "Admin" ? (
-                <Navigate to="/admin" />
-              ) : (
-                <Navigate to="/" />
-              )
-            }
-          />
-          <Route
-            path="/signup"
-            element={!user ? <Signup /> : <Navigate to="/" />}
-          />
-          <Route path="/courses" element={<Courses />} />
-          <Route
-            path="/course/create/add"
-            element={
-              user && user.role === "Admin" ? (
-                <AddCourse />
-              ) : (
-                <Navigate to="/" />
-              )
-            }
-          />
 
-          <Route path="/sabbathSchool" element={<SabbathSchool />} />
-          <Route path="/devotion" element={<Devotion />} />
-          <Route path="/aboutUs" element={<AboutUs />} />
-          <Route path="/contactUs" element={<ContactUs />} />
-          <Route path="/logIn" element={<LogIn />} />
-          <Route path="/createAccount" element={<CreateAccount />} />
-          <Route path="/courses/get/:courseId" element={<ChaptersDisplay />} />
-          <Route
-            path="/courses/get/:courseId/chapter/:chapterId"
-            element={<SlidesDisplay />}
-          />
-          <Route path="/courses/create/chapters" element={<AdminChapter />} />
-          <Route path="/courses/create/add" element={<AddCourse />} />
-          <Route path="*" element={<NotMatch />} />
-          <Route
-            path="/admin"
-            element={
-              user && user.role === "Admin" ? (
-                <AdminDashboard />
-              ) : (
-                <Navigate to="/" />
-              )
-            }
-          />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+  // Private Route for Admin
+  const PrivateAdminRoute = ({ children }) => {
+    return user && user.role === "Admin" ? children : <Navigate to="/" />;
+  };
+
+  PrivateAdminRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+  // Public Route (redirect if logged in)
+
+  const PublicRoute = ({ children }) => {
+    return !user ? children : <Navigate to="/" />;
+  };
+
+  PublicRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            user && user.role === "Admin" ? <Navigate to="/admin" /> : <Home />
+          }
+        />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/sabbathSchool" element={<SabbathSchool />} />
+        <Route path="/devotion" element={<Devotion />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/courses/get/:courseId" element={<ChaptersDisplay />} />
+        <Route
+          path="/courses/get/:courseId/chapter/:chapterId"
+          element={<SlidesDisplay />}
+        />
+        <Route path="*" element={<NotMatch />} />
+
+        {/* Protected Routes for Admin */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateAdminRoute>
+              <AdminDashboard />
+            </PrivateAdminRoute>
+          }
+        />
+        <Route
+          path="/course/create/add"
+          element={
+            <PrivateAdminRoute>
+              <AddCourse />
+            </PrivateAdminRoute>
+          }
+        />
+        <Route
+          path="/courses/create/chapters"
+          element={
+            <PrivateAdminRoute>
+              <AdminChapter />
+            </PrivateAdminRoute>
+          }
+        />
+
+        {/* Public Routes (Redirect if logged in) */}
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
+          }
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/routes/AdminDashboard.jsx
+++ src/routes/AdminDashboard.jsx
@@ -1,16 +1,31 @@
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import Courses from "../routes/Courses";
+import SabbathSchool from "../routes/SabbathSchool";
+import Devotion from "../routes/Devotion";
+import AddCourse from "../components/AddCourse";
+import ChaptersDisplay from "../features/CourseComponents/ChaptersDisplay";
+import SlidesDisplay from "../features/CourseComponents/SlidesDisplay";
+import AdminChapter from "../features/CourseComponents/AdminChapter";
 
 const AdminDashboard = () => {
   return (
     <div>
       <Sidebar />
       <Routes>
-        {/* Add your admin routes here */}
         <Route path="/" element={<div>Admin Home</div>} />
-        <Route path="/item-1" element={<div>Item 1 Page</div>} />
-        <Route path="/item-2" element={<div>Item 2 Page</div>} />
-        <Route path="/item-3" element={<div>Item 3 Page</div>} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/course/create/add" element={<AddCourse />} />
+        <Route path="/sabbathSchool" element={<SabbathSchool />} />
+        <Route path="/devotion" element={<Devotion />} />
+        <Route path="/courses/get/:courseId" element={<ChaptersDisplay />} />
+        <Route
+          path="/courses/get/:courseId/chapter/:chapterId"
+          element={<SlidesDisplay />}
+        />
+        <Route path="/courses/create/chapters" element={<AdminChapter />} />
+        <Route path="/courses/create/add" element={<AddCourse />} />
+        {/* Add more routes as needed */}
       </Routes>
     </div>
   );
