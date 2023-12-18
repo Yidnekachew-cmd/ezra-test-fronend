fd3808a2e85fe01234e087e5fcf2fe2fccd41f75
Yidne <yamiyekn@gmail.com>
2023-12-18 10:21:09 +0300

    Uncomment courseAvailable component.

--- src/routes/Courses.jsx
+++ src/routes/Courses.jsx
@@ -1,43 +1,43 @@
-// // import { useEffect, useState } from "react";
-// // import { useNavigate } from "react-router-dom";
-// // import CreateCourse from "@/components/CreateCourse";
-// import CoursesAvailable from "@/features/CourseComponents/CoursesAvailable";
-// // import AddCourse from "../components/AddCourse";
-// // import { useAuthContext } from "../hooks/useAuthContext";
-
-// const Courses = () => {
-//   // const [step, setStep] = useState(1);
-//   // const [courseData, setCourseData] = useState(null);
-//   // const navigate = useNavigate();
-//   // const { role } = useAuthContext();
-
-//   // useEffect(() => {
-//   //   if (step === 2) {
-//   //     navigate("/course/create/add");
-//   //   }
-//   // }, [step]);
-
-//   return (
-//     <div>
-//       <CoursesAvailable />
-//       {/* {step === 1 && role === "Admin" && (
-//         <CreateCourse setCourseData={setCourseData} setStep={setStep} />
-//       )} */}
-//       {/* {step === 2 && role === "Admin" && <AddCourse courseData={courseData} />} */}
-//     </div>
-//   );
-// };
-
-// export default Courses;
-
-import CreateCourse from "@/components/CreateCourse";
+// import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import CreateCourse from "@/components/CreateCourse";
+import CoursesAvailable from "@/features/CourseComponents/CoursesAvailable";
+// import AddCourse from "../components/AddCourse";
+// import { useAuthContext } from "../hooks/useAuthContext";
 
 const Courses = () => {
+  // const [step, setStep] = useState(1);
+  // const [courseData, setCourseData] = useState(null);
+  // const navigate = useNavigate();
+  // const { role } = useAuthContext();
+
+  // useEffect(() => {
+  //   if (step === 2) {
+  //     navigate("/course/create/add");
+  //   }
+  // }, [step]);
+
   return (
     <div>
-      <CreateCourse />
+      <CoursesAvailable />
+      {/* {step === 1 && role === "Admin" && (
+        <CreateCourse setCourseData={setCourseData} setStep={setStep} />
+      )} */}
+      {/* {step === 2 && role === "Admin" && <AddCourse courseData={courseData} />} */}
     </div>
   );
 };
 
 export default Courses;
+
+// import CreateCourse from "@/components/CreateCourse";
+
+// const Courses = () => {
+//   return (
+//     <div>
+//       <CreateCourse />
+//     </div>
+//   );
+// };
+
+// export default Courses;
