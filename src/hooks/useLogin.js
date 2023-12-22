25c0ef17e197246c00d7dfb2b18159058e47b5a6
Besufekad-HAZ <besurye@gmail.com>
2023-12-22 13:30:28 +0300

    Fix Conflicts

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -26,7 +26,7 @@ export const useLogin = () => {
       if (response.ok) {
         // save the user to local storage
         localStorage.setItem("user", JSON.stringify(json));
-        console.log(json);
+        // console.log(json);
 
         // update the auth context
         dispatch({ type: "LOGIN", payload: json });
