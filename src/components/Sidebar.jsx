c157e89986885a6059bdbdb986e339bb9571c419
Besufekad-HAZ <besurye@gmail.com>
2023-12-22 13:44:34 +0300

    Updating padding and design in the sidebar components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,20 +16,20 @@ const Sidebar = () => {
   return (
     <div className="flex flex-col bg-gray-800 text-white h-screen">
       <div
-        className="px-4 py-3 cursor-pointer hover:bg-accent-6"
+        className="px-4 py-5 cursor-pointer hover:bg-accent-6"
         onClick={() => handleMenuClick("courses")}
       >
         Courses
         {openMenu === "courses" && (
-          <ul className="pl-4 mt-2">
+          <ul className="pl-4 mt-2 py-2">
             <li
-              className="cursor-pointer hover:text-gray-300"
+              className="cursor-pointer hover:text-gray-300 mb-2"
               onClick={() => handleSubItemClick("/admin/courses")}
             >
               Create Course
             </li>
             <li
-              className="cursor-pointer hover:text-gray-300"
+              className="cursor-pointer hover:text-gray-300 mb-2"
               onClick={() => handleSubItemClick("/admin/courses/create/add")}
             >
               Manage Courses
@@ -39,38 +39,38 @@ const Sidebar = () => {
         )}
       </div>
       <div
-        className="px-4 py-3 cursor-pointer  hover:bg-accent-6"
+        className="px-4 py-5 cursor-pointer  hover:bg-accent-6"
         onClick={() => handleMenuClick("sabbathSchool")}
       >
         Sabbath School
         {openMenu === "sabbathSchool" && (
-          <ul className="pl-4 mt-2">
+          <ul className="pl-4 mt-2 py-2">
             <li
-              className="cursor-pointer hover:text-gray-300"
+              className="cursor-pointer hover:text-gray-300 mb-2"
               onClick={() => handleSubItemClick("/admin/sabbathSchool")}
             >
-              View Sabbath School
+              Create Sabbath School
             </li>
             {/* Add more sub-items as needed */}
           </ul>
         )}
       </div>
       <div
-        className="px-4 py-3 cursor-pointer  hover:bg-accent-6"
+        className="px-4 py-5 cursor-pointer  hover:bg-accent-6"
         onClick={() => handleMenuClick("devotion")}
       >
         Devotion
         {openMenu === "devotion" && (
-          <ul className="pl-4 mt-2">
+          <ul className="pl-4 mt-2 py-2">
             <li
-              className="cursor-pointer hover:text-gray-300"
+              className="cursor-pointer hover:text-gray-300 mb-2"
               onClick={() => handleSubItemClick("/admin/devotion/create")}
             >
               Add Devotion
             </li>
 
             <li
-              className="cursor-pointer hover:text-gray-300"
+              className="cursor-pointer hover:text-gray-300 mb-2"
               onClick={() => handleSubItemClick("/admin/devotion")}
             >
               Manage Devotion
