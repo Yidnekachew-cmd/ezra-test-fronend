a2b3e41b1898d7f412d4c158b5e7195456723384
yeabtsega45 <yabtsega2022@gmail.com>
2024-01-19 17:49:47 +0300

    change the api endpoint

--- src/services/coursesApi.js
+++ src/services/coursesApi.js
@@ -4,12 +4,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const coursesApi = createApi({
   reducerPath: "coursesApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5100/",
-    // baseUrl: "https://ezra-seminary-api.onrender.com/",
+    // baseUrl: "http://localhost:5100/",
+    baseUrl: "https://ezra-seminary-api.onrender.com/",
   }),
   endpoints: (builder) => ({
     getCourses: builder.query({
-      query: () => "courses",
+      query: () => "course/getall",
     }),
   }),
 });
