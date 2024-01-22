6040de497e0d763a117694b37236d3e19b22ead7
yeabtsega45 <yabtsega2022@gmail.com>
2024-01-22 20:47:52 +0300

    changing get single course to rtk query

---
+++ src/features/CourseComponents/ChaptersDisplayUseffect.jsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+
+function ChaptersDisplayUseffect() {
+  const [data, setData] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [unlockedIndex, setUnlockedIndex] = useState(0); // New state variable to track the unlocked index
+
+  const { courseId } = useParams();
+
+  //get single course
+  useEffect(() => {
+    axios
+      .get(`/course/get/${courseId}`)
+      .then((res) => {
+        setData(res.data.chapters);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [courseId]);
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= data.length) {
+      newIndex = data.length - 1;
+    }
+
+    if (newIndex > unlockedIndex) {
+      setUnlockedIndex(newIndex); // Update the unlocked index
+    }
+
+    setActiveIndex(newIndex);
+  };
+
+  // slide number
+  const currentDataNumber = activeIndex + 1;
+  const totalDataNumber = data.length;
+
+  const isSlideUnlocked = (index) => {
+    return index <= unlockedIndex; // Check if the slide is unlocked based on the unlocked index
+  };
+
+  return (
+    <div className="flex justify-center items-center w-[80%] mx-auto">
+      <div className="flex w-[100%] justify-center items-center h-screen ">
+        <div className="flex flex-col justify-start items-center md:w-[30%] h-[80%] shadow-2xl  rounded-lg border-2 border-accent-5 ">
+          {/* slide number */}
+          <div className="w-[90%] mx-auto ">
+            <div className="flex flex-col mt-6 border-accent-5 border-1">
+              <h1 className=" font-Lato-Black pb-1">
+                CHAPTER {currentDataNumber}/{totalDataNumber}
+              </h1>
+              <hr className="border-accent-5 border-1 w-[100%] mx-auto" />
+            </div>
+            <div className="flex flex-col  mt-[20px]">
+              {data.map((chapter, index) => {
+                const unlocked = isSlideUnlocked(index);
+                return (
+                  <button
+                    key={index}
+                    className={`flex justify-between items-center text-sm font-nokia-bold border-b-2 border-accent-5 px-4 text-secondary-6 cursor-pointer py-2 ${
+                      unlocked ? "text-black" : "text-gray-500"
+                    }  ${index === activeIndex && "font-bold bg-[#FAE5C7]"}
+                    `} // Locked slide to gray
+                    onClick={() => {
+                      updateIndex(index);
+                    }}
+                  >
+                    <span>{chapter.chapter}</span>
+                    {unlocked ? (
+                      <span className="material-symbols-outlined text-accent-6 pl-4 text-xl">
+                        check_circle
+                      </span>
+                    ) : (
+                      <span className="material-symbols-outlined text-accent-6 pl-4 text-lg">
+                        radio_button_unchecked
+                      </span>
+                    )}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        {/* slides */}
+        <div className=" md:w-[70%] justify-start items-center mx-auto h-[80%]  bg-chapter-img-1 bg-no-repeat bg-cover bg-center rounded-lg ">
+          <div className="flex flex-col justify-between h-full">
+            <div>
+              <div className="w-[90%] pt-4 pb-2 flex justify-between mx-auto items-center">
+                <h1 className="text-[#fff] text-sm font-Lato-Black">
+                  EZRA seminary
+                </h1>
+                <img
+                  src="../../assets/close-icon.svg"
+                  className="w-[3%] z-40 cursor-pointer"
+                  alt=""
+                />
+              </div>
+              <hr className="border-accent-5 border-1 w-[90%] mx-auto" />
+            </div>
+            {data.map((chapter, index) => {
+              if (index === activeIndex) {
+                return (
+                  <div
+                    key={index}
+                    className="flex flex-col justify-center py-52 flex-grow"
+                  >
+                    <h1 className="text-3xl text-[#fff] text-center font-nokia-bold">
+                      {chapter.chapter}
+                    </h1>
+                    <button className="text-white text-center font-nokia-bold mt-2 py-2 px-4 bg-accent-6 hover:bg-accent-7 w-[20%] rounded-3xl mx-auto text-2xl ">
+                      <Link
+                        to={`/courses/get/${courseId}/chapter/${chapter._id}`}
+                      >
+                        ትምህርቱን ጀምር
+                      </Link>
+                    </button>
+                  </div>
+                );
+              } else {
+                return null; // Hide the chapter if it doesn't match the activeIndex
+              }
+            })}
+            <div>
+              <hr className="border-accent-5 border-1 w-[90%] mx-auto mb-8" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChaptersDisplayUseffect;
---
+++ src/features/CourseComponents/SlidesDisplayUseffect.jsx
@@ -0,0 +1,254 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import { CaretCircleLeft } from "@phosphor-icons/react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import "@splidejs/react-splide/css/sea-green";
+import "@splidejs/react-splide/css/core";
+
+function SlidesDisplayUseffect() {
+  const [data, setData] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [unlockedIndex, setUnlockedIndex] = useState(0); // New state variable to track the unlocked index
+
+  const { courseId, chapterId } = useParams(); // Note the two separate parameters
+
+  //get all courses
+  useEffect(() => {
+    axios
+      .get(`/course/get/${courseId}`) // Assuming you will change the endpoint as needed
+      .then((res) => {
+        // Now we need to find the specific chapter within the course
+        const chapter = res.data.chapters.find(
+          (chap) => chap._id === chapterId
+        );
+        if (chapter) {
+          setData(chapter.slides);
+        } else {
+          console.log("Chapter not found");
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [courseId, chapterId]);
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= data.length) {
+      newIndex = data.length - 1;
+    }
+
+    if (newIndex > unlockedIndex) {
+      setUnlockedIndex(newIndex); // Update the unlocked index
+    }
+
+    setActiveIndex(newIndex);
+  };
+
+  // slide number
+  const currentDataNumber = activeIndex + 1;
+  const totalDataNumber = data.length;
+
+  const isSlideUnlocked = (index) => {
+    return index <= unlockedIndex; // Check if the slide is unlocked based on the unlocked index
+  };
+
+  return (
+    <div className="flex justify-center items-center w-[80%] mx-auto">
+      <div className="flex w-[100%] justify-center items-center h-screen ">
+        <div className="flex flex-col justify-start items-center md:w-[30%] h-[80%] shadow-2xl  rounded-lg border-2 border-accent-5 ">
+          {/* slide number */}
+          <div className="w-[90%] mx-auto ">
+            <div className="flex flex-col mt-6 border-accent-5 border-1">
+              <h1 className=" font-Lato-Black pb-1">
+                SLIDE {currentDataNumber}/{totalDataNumber}
+              </h1>
+              <hr className="border-accent-5 border-1 w-[100%] mx-auto" />
+            </div>
+            <div className="flex flex-col  mt-[20px]">
+              {data.map((slides, index) => {
+                const unlocked = isSlideUnlocked(index);
+                return (
+                  <button
+                    key={index}
+                    className={`flex justify-between items-center text-sm font-nokia-bold border-b-2 border-accent-5 px-4 text-secondary-6 cursor-pointer py-2 ${
+                      unlocked ? "text-black" : "text-gray-500"
+                    }  ${index === activeIndex && "font-bold bg-[#FAE5C7]"}
+                    `}
+                    onClick={() => {
+                      updateIndex(index);
+                    }}
+                    disabled={!unlocked} // Disable the button if the slide is locked
+                  >
+                    <span>{slides.slide}</span>
+                    {unlocked ? (
+                      <span className="material-symbols-outlined text-accent-6 pl-4 text-xl">
+                        check_circle
+                      </span>
+                    ) : (
+                      <span className="material-symbols-outlined text-accent-6 pl-4 text-lg">
+                        radio_button_unchecked
+                      </span>
+                    )}
+                  </button>
+                );
+              })}
+            </div>
+            <Link to={`/courses/get/${courseId}`}>
+              <div className="flex justify-between items-center w-[100%] mx-auto mt-12">
+                <button className="text-white font-nokia-bold bg-accent-6 hover:bg-accent-7 rounded-xl py-1 px-4 transition-all">
+                  ዘግተህ ውጣ
+                </button>
+                <CaretCircleLeft className="text-2xl bg-accent-6 rounded-full text-primary-1 mr-2 hover:bg-accent-7 transition-all" />
+              </div>
+            </Link>
+          </div>
+        </div>
+        {/* slides */}
+        <div className=" md:w-[70%] justify-start items-center mx-auto h-[80%]  bg-chapter-img-1 bg-no-repeat bg-cover bg-center rounded-lg">
+          <div className="flex flex-col justify-between h-full">
+            <div>
+              <div className="w-[90%] pt-4 pb-2 flex justify-between mx-auto items-center">
+                <h1 className="text-[#fff] text-sm font-Lato-Black">
+                  EZRA seminary
+                </h1>
+                <img
+                  src="../assets/close-icon.svg"
+                  className="w-[3%] z-40 cursor-pointer"
+                  alt=""
+                />
+              </div>
+              <hr className="border-accent-5 border-1 w-[90%] mx-auto" />
+            </div>
+            <div>
+              {data.map((slides, index) => {
+                if (index === activeIndex) {
+                  return (
+                    <div
+                      key={index}
+                      className="flex flex-col justify-center flex-grow w-[80%] mx-auto"
+                    >
+                      <h1 className="text-3xl text-[#fff] text-center font-nokia-bold">
+                        {slides.slide}
+                      </h1>
+                      {slides.elements.map((element) => {
+                        if (element.type === "title") {
+                          return (
+                            <li
+                              key={element._id}
+                              className="text-white text-3xl font-nokia-bold pl-20"
+                            >
+                              {element.value}
+                            </li>
+                          );
+                        } else if (element.type === "sub") {
+                          return (
+                            <p
+                              key={element._id}
+                              className="text-white font-nokia-bold  self-center tracking-wide text-center text-2xl mt-2"
+                            >
+                              {element.value}
+                            </p>
+                          );
+                        } else if (element.type === "text") {
+                          return (
+                            <p
+                              key={element._id}
+                              className="text-white font-nokia-bold   self-center tracking-wide text-justify text-lg mt-2"
+                            >
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{element.value}
+                            </p>
+                          );
+                        } else if (element.type === "img") {
+                          return (
+                            <img
+                              key={element._id}
+                              // src={`https://ezra-seminary-api.onrender.comimages/${element.value}`}
+                              src={`https://ezra-seminary-api.onrender.com-api.onrender.com//images/${element.value}`}
+                              alt={element.id}
+                              className="w-[30%] mx-auto border border-accent-6 shadow-xl padding mt-2"
+                            />
+                          );
+                        } else if (element.type === "list") {
+                          const listItemsComponent = element.value.map(
+                            (listItem, index) => (
+                              <li
+                                key={index}
+                                className="text-white font-nokia-bold w-[100%] tracking-wide text-left text-lg"
+                              >
+                                {listItem}
+                              </li>
+                            )
+                          );
+
+                          return (
+                            <div
+                              key={element._id}
+                              className="flex flex-col ml-8"
+                            >
+                              <ul className="list-disc mt-2">
+                                {listItemsComponent}
+                              </ul>
+                            </div>
+                          );
+                        } else if (element.type === "slide") {
+                          const listItemsComponent = element.value.map(
+                            (listItem, index) => (
+                              <SplideSlide
+                                key={index}
+                                className="text-white font-nokia-bold w-[100%] tracking-wide text-left text-lg px-8"
+                              >
+                                {listItem}
+                              </SplideSlide>
+                            )
+                          );
+
+                          return (
+                            <div
+                              key={element._id}
+                              className="flex flex-col ml-8"
+                            >
+                              <Splide
+                                options={{
+                                  gap: "1rem",
+                                }}
+                                className="bg-accent-6 p-8 rounded-md list-disc mt-2"
+                              >
+                                {listItemsComponent}
+                              </Splide>
+                            </div>
+                          );
+                        } else {
+                          return null;
+                        }
+                      })}
+                    </div>
+                  );
+                } else {
+                  return null; // Hide the slide if it doesn't match the activeIndex
+                }
+              })}
+            </div>
+            <div className="mb-4">
+              <hr className="border-accent-5  border-1 w-[90%] mx-auto z-50 " />
+              <button
+                className={`text-white text-center font-nokia-bold mt-2 py-1 px-2 bg-accent-6 hover:bg-accent-7 w-[15%] rounded-3xl mx-auto text-2xl transition-all ${
+                  activeIndex === data.length - 1 ? "hidden" : "block"
+                }`} // hidding the next button for the last slide
+                onClick={() => {
+                  updateIndex(activeIndex + 1);
+                }}
+              >
+                ቀጥል
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      ;
+    </div>
+  );
+}
+
+export default SlidesDisplayUseffect;
