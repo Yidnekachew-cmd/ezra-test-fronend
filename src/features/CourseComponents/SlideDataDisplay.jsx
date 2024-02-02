ef190e538e0f5f8da1e2d65e05ff6159b18e615c
yeabtsega45 <yabtsega2022@gmail.com>
2024-02-02 22:38:03 +0300

    styling Quiz, Slides & List items elements

--- src/features/CourseComponents/SlideDataDisplay.jsx
+++ src/features/CourseComponents/SlideDataDisplay.jsx
@@ -74,6 +74,9 @@ function SlideDataDisplay({ selectedSlideIndex }) {
         </div>
         {selectedSlide && selectedSlide.elements && (
           <div className="flex flex-col justify-center flex-grow p-20">
+            <h1 className="text-3xl text-[#fff] text-center font-nokia-bold">
+              {selectedSlide.slide}
+            </h1>
             <ul>
               {selectedSlide.elements.map((element, index) => {
                 let elementComponent = null;
@@ -119,7 +122,7 @@ function SlideDataDisplay({ selectedSlideIndex }) {
                   );
 
                   elementComponent = (
-                    <div className="flex flex-col ml-8">
+                    <div className="flex flex-col justify-center items-center ml-8">
                       <ul className="list-disc mt-2">{listItemsComponent}</ul>
                     </div>
                   );
