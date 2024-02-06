99d7cc685653fcb8d78f2f72804281bff14a9e54
AmanWorku <amexworku2014@gmail.com>
2024-02-06 16:52:47 +0300

    Create layout

--- src/features/SabbathSchoolComponent/SSLHome.jsx
+++ src/features/SabbathSchoolComponent/SSLHome.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useGetSSLsQuery } from "./../../services/SabbathSchoolApi"; // Ensure this path is correct
 
 const SSLHome = () => {
@@ -6,24 +7,32 @@ const SSLHome = () => {
 
   if (error) return <div>Error: {error.message}</div>;
   if (isLoading) return <div>Loading...</div>;
-  console.log(ssl);
 
   return (
-    <div className="h-auto flex flex-row w-[90%] md:w-[80%] mt-12 mx-auto space-y-12 mb-12">
-      SSLHome
-      {ssl &&
-        ssl.map((item, index) => (
-          <div
+    <div className="container mx-auto px-4 w-[90%] md:w-[80%] py-12 font-nokia-bold ">
+      <h1 className="text-3xl mb-6">Sabbath School Lessons</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {ssl.map((item, index) => (
+          <Link
             key={index}
-            className="border border-accent-6 p-2 rounded-lg font-nokia-bold w-1/3"
+            className="flex bg-white shadow-md rounded-md border border-accent-6"
           >
-            <img src={item.cover} alt="" className="w-48 rounded-md" />
-            <div>
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
+            <img
+              src={item.cover}
+              alt={item.title}
+              className="rounded-l-md h-42 w-1/2"
+            />
+            <div className="flex flex-col justify-between p-4">
+              <div className="flex flex-col h-48">
+                <h2 className="text-xl mb-2 whitespace-normal">{item.title}</h2>
+                <p className="text-gray-600 overflow-hidden overflow-ellipsis">
+                  {item.description}
+                </p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
+      </div>
     </div>
   );
 };
