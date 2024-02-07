095683a9c9bdd5667f43332accc0834317f386a0
AmanWorku <amexworku2014@gmail.com>
2024-02-07 11:39:20 +0300

    Fix layout for displaying list of days

--- src/features/SabbathSchoolComponent/SSLDay.jsx
+++ src/features/SabbathSchoolComponent/SSLDay.jsx
@@ -15,10 +15,10 @@ function SSLDay() {
   return (
     <div className="container mx-auto px-4 w-[90%] md:w-[80%] py-12 font-nokia-bold ">
       <h1 className="text-3xl mb-6">Sabbath School Lessons</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="">
         {lessonDetails.days.map((item, index) => (
           <div className="flex flex-col justify-between p-4" key={index}>
-            <div className="flex flex-col h-48">
+            <div className="flex flex-col">
               <h2 className="text-xl mb-2 whitespace-normal">{item.title}</h2>
               <p className="text-gray-600 overflow-hidden overflow-ellipsis">
                 {item.date}
