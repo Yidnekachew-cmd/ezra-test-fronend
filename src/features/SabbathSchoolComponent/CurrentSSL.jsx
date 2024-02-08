0e3987dfeda4244b6439d5649c2f60a9de6ccde2
AmanWorku <amexworku2014@gmail.com>
2024-02-08 15:43:09 +0300

    Update styling and add content to the section

--- src/features/SabbathSchoolComponent/CurrentSSL.jsx
+++ src/features/SabbathSchoolComponent/CurrentSSL.jsx
@@ -5,6 +5,7 @@ import {
   useGetSSLOfQuarterQuery,
 } from "./../../services/SabbathSchoolApi";
 import DateConverter from "./DateConverter";
+import { YoutubeLogo } from "@phosphor-icons/react";
 function CurrentSSL() {
   const currentDate = new Date().toISOString().slice(0, 10);
   const [quarter, week] = useCalculateLessonIndex(currentDate);
@@ -34,16 +35,16 @@ function CurrentSSL() {
 
   return (
     <div className="container mx-auto px-4 w-[90%] md:w-[80%] py-12 font-nokia-bold text-secondary-6">
-      <div className="w-full border border-accent-6 p-2 rounded-xl">
+      <div className="flex gap-4 w-full border border-accent-6 p-2 rounded-xl">
         <div
-          className="relative rounded-md w-64 h-48 text-primary-1 p-4 items-end"
+          className="flex rounded-md w-[35%] h-48 text-primary-1 p-4 items-end"
           style={{
-            backgroundImage: `linear-gradient(to top, rgba(234, 146, 21, 10) 0%, rgba(234, 146, 21, 0) 80%), url(${backgroundImage})`,
+            backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0.2) 0%, rgba(234,146,21,0.8) 100%), url(${backgroundImage})`,
             backgroundSize: "cover",
           }}
         >
-          <div className="mx-auto">
-            <p className="">የዚህ ሳምንት ትምህርት</p>
+          <div className="">
+            <p>የዚህ ሳምንት ትምህርት</p>
             {quarterDetails && quarterDetails.quarterly && (
               <div className="flex text-2xl text-primary-3">
                 <DateConverter
@@ -54,6 +55,31 @@ function CurrentSSL() {
               </div>
             )}
           </div>
+        </div>
+        <div className="flex flex-col w-[65%]">
+          <div className="flex w-full justify-between">
+            <div className="">
+              <p className="text-lg text-accent-6">
+                {quarterDetails.quarterly.title}
+              </p>
+              <p className="text-3xl text-secondary-6 leading-8">
+                {lessonDetails.lesson.title}
+              </p>
+              <p className="text-xs text-accent-6 mt-2">
+                {quarterDetails.quarterly.human_date}
+              </p>
+              <div className="border border-b-accent-6 my-2" />
+            </div>
+            <div className="flex flex-col gap-2 items-end">
+              <button className="px-4 py-1 bg-accent-6 text-primary-1 rounded-full text-xs">
+                ትምህርቱን ክፈት
+              </button>
+              <button className="px-2 border border-accent-6 text-accent-6 text-xs flex rounded-full items-center gap-2">
+                Watch on YouTube <YoutubeLogo size={24} weight="fill" />
+              </button>
+            </div>
+          </div>
+          <p className="text-xs">{quarterDetails.quarterly.description}</p>
         </div>
       </div>
     </div>
