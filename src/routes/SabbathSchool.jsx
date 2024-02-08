2154770d10229863ecabe8f85cd5cc36cb2f4a39
AmanWorku <amexworku2014@gmail.com>
2024-02-08 16:16:45 +0300

    Import searching bar

--- src/routes/SabbathSchool.jsx
+++ src/routes/SabbathSchool.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import SSLHome from "@/features/SabbathSchoolComponent/SSLHome";
 import CurrentSSL from "@/features/SabbathSchoolComponent/CurrentSSL";
+import { MagnifyingGlass } from "@phosphor-icons/react";
 
 const SabbathSchool = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showInput, setShowInput] = useState(false);
+
+  {
+    /* function to handle search input */
+  }
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  {
+    /* function to handle search icon click */
+  }
+  const handleSearchIconClick = () => {
+    setShowInput(!showInput);
+  };
+
+  {
+    /* Check if the screen is small */
+  }
+  const isSmallScreen = window.matchMedia("(max-width: 426px)").matches;
   return (
     <div className="container mx-auto px-4 w-[90%] md:w-[80%] py-12 font-nokia-bold text-secondary-6">
       <CurrentSSL />
@@ -13,7 +36,46 @@ const SabbathSchool = () => {
               Lessons of previous quarters
             </p>
           </div>
-          <div></div>
+          <div>
+            {/* Search bar */}
+            <div className="flex justify-between items-center">
+              {isSmallScreen ? (
+                <div className="flex items-center justify-end">
+                  {showInput && (
+                    <input
+                      type="text"
+                      placeholder="Search"
+                      value={searchTerm}
+                      onChange={handleSearch}
+                      className="text-xs text-secondary-6 border border-accent-6 w-[50%] outline-1 outline-accent-5 rounded-l-lg  px-2 py-1"
+                    />
+                  )}
+                  <span
+                    className="cursor-pointer border  rounded-r-lg px-1 py-[0.4rem] -ml-1 bg-accent-5 text-white block"
+                    onClick={handleSearchIconClick}
+                  >
+                    <MagnifyingGlass size={20} />
+                  </span>
+                </div>
+              ) : (
+                <div className="flex  ">
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    value={searchTerm}
+                    onChange={handleSearch}
+                    className="text-xs text-secondary-6 border border-accent-6 w-auto outline-1 outline-accent-5 rounded-l-lg  px-2 py-1"
+                  />
+                  <span
+                    className=" self-center cursor-pointer border  rounded-r-lg px-1 py-[0.54rem] -ml-1 bg-accent-5 text-white"
+                    onClick={handleSearchIconClick}
+                  >
+                    <MagnifyingGlass size={20} />
+                  </span>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
         <div className="border border-b-accent-6 " />
       </div>
