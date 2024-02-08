da72c0aaebae9eaec334d167c819d68f5243b095
AmanWorku <amexworku2014@gmail.com>
2024-02-08 13:48:53 +0300

    Add a styling for the date section

--- src/features/SabbathSchoolComponent/CurrentSSL.jsx
+++ src/features/SabbathSchoolComponent/CurrentSSL.jsx
@@ -1,16 +1,14 @@
-// CurrentSSL.jsx
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import useCalculateLessonIndex from "./hooks/useCalculateLessonIndex";
 import {
   useGetSSLOfDayQuery,
   useGetSSLOfQuarterQuery,
 } from "./../../services/SabbathSchoolApi";
+import DateConverter from "./DateConverter";
 function CurrentSSL() {
-  // Fetch current date
-  const currentDate = new Date().toISOString().slice(0, 10); // Get current date in YYYY-MM-DD format
-
-  // Get the lesson index from the hook
+  const currentDate = new Date().toISOString().slice(0, 10);
   const [quarter, week] = useCalculateLessonIndex(currentDate);
+  const [backgroundImage, setBackgroundImage] = useState("");
 
   const {
     data: lessonDetails,
@@ -23,6 +21,12 @@ function CurrentSSL() {
     isLoading: quarterIsLoading,
   } = useGetSSLOfQuarterQuery(quarter);
 
+  useEffect(() => {
+    if (lessonDetails) {
+      setBackgroundImage(lessonDetails.lesson.cover);
+    }
+  }, [lessonDetails]);
+
   if (lessonIsLoading || quarterIsLoading) return <div>Loading...</div>;
   if (lessonError) return <div>Error: {lessonError.message}</div>;
   if (quarterError) return <div>Error: {quarterError.message}</div>;
@@ -31,12 +35,26 @@ function CurrentSSL() {
   return (
     <div className="container mx-auto px-4 w-[90%] md:w-[80%] py-12 font-nokia-bold text-secondary-6">
       <div className="w-full border border-accent-6 p-2 rounded-xl">
-        <img
-          src={lessonDetails.lesson.cover}
-          alt={quarterDetails.quarterly.title}
-          className="rounded-md w-20% h-48 object-contain"
-        />
-        {quarterDetails.quarterly.title}
+        <div
+          className="relative rounded-md w-64 h-48 text-primary-1 p-4 items-end"
+          style={{
+            backgroundImage: `linear-gradient(to top, rgba(234, 146, 21, 10) 0%, rgba(234, 146, 21, 0) 80%), url(${backgroundImage})`,
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="mx-auto">
+            <p className="">የዚህ ሳምንት ትምህርት</p>
+            {quarterDetails && quarterDetails.quarterly && (
+              <div className="flex text-2xl text-primary-3">
+                <DateConverter
+                  gregorianDate={lessonDetails.lesson.start_date}
+                />
+                &nbsp;- &nbsp;
+                <DateConverter gregorianDate={lessonDetails.lesson.end_date} />
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
